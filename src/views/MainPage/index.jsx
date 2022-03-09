607011a35ff57aa2098cca459d0bca987e039f41
bruno <bru_fofo@hotmail.com>
2022-03-09 10:55:03 -0300

    save

---
+++ src/views/MainPage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "../../components/Button/Button";
+
+
+const MainPage = () => {
+    return     (<><Button value='City 1'></Button>
+    <Button value='City 2'></Button>
+    <Button value='City 3'></Button></>)
+}
+
+export default MainPage;
